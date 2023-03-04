ef19a61d192833c75b844823fb16990055e3d241
Aleena Nadeem <95051612+Aleena-Nadeem@users.noreply.github.com>
2023-03-05 04:16:37 +0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const routes = require('./src/routes/routes.js');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
+    port: process.env.PORT || 3000,
     host: 'localhost'
   });
 
